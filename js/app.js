b7c6305fef145e3002333fd0f6bf4f0775f69dc3
Norberto R. Sigel <norbertosigel@gmail.com>
2024-01-22 21:35:02 -0300

    Avance del proyecto, se agregó dos secciones, hero section y about section. Falta agregar imagen en about section.

--- js/app.js
+++ js/app.js
@@ -16,3 +16,24 @@ menuLinks.forEach((menuLink) => {
     navbarMenu.classList.remove('active');
   })
 });
+
+//Change backgound header
+function scrollHeader() { 
+  const header = document.getElementById('header');
+  this.scrollY >= 20 ? header.classList.add('active') : header.classList.remove('active');
+}
+window.addEventListener('scroll', scrollHeader);
+
+//Hero type effect
+const typed = document.querySelector('.typed');
+if (typed) {
+  let typed_strings = typed.getAttribute('data-typed-items');
+  typed_strings = typed_strings.split(',');
+  new Typed('.typed', {
+    strings: typed_strings,
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 50,
+    backDelay: 2000
+  })
+}
